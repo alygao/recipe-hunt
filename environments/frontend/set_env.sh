7e12a55b56348ac838a539cab1ddd20782f1d8bc
alyssagao1120 <alyssa.gao@yahoo.ca>
2022-01-02 19:26:33 -0500

    fixed set_env.sh for frontent

--- environments/frontend/set_env.sh
+++ environments/frontend/set_env.sh
@@ -1,4 +1,4 @@
 echo "export const environment = {
-   apiURL: 'http://${ }',
+   apiURL: 'http://${API_URL_HOST}:${API_URL_PORT}',
    production: true,
 };" > /usr/local/app/recipe-hunt/'Frontend (Angular)'/src/environments/environment.ts
